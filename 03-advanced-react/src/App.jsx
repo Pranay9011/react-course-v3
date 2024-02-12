1473ba92c2d4245d6dcd4b8f6aaf7995bf364701
Pranay9011 <patilpranay76@gmail.com>
2024-02-13 01:57:13 +0530

    challenge completed

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,8 +1,8 @@
+import UserChallenge from "./tutorial/06-forms/starter/02-user-challenge";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
-    </div>
+    <UserChallenge />
   );
 }
 
--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,4 +1,27 @@
+import React, { useState } from "react";
+import { data } from "../../../data"
+
+
 const UserChallenge = () => {
+  const [data1, setData1] = useState(data)
+  const [name, setName] = useState("")
+  const [nextID, setNextID] = useState(data.length + 1)
+
+  const addUser = (name) => {
+    debugger
+    if (!name) {
+      alert("Please enter name")
+      return;
+    }
+    const newObj = { id: nextID, name: name };
+    setData1([...data1, newObj]);
+    setNextID(nextID + 1);
+    setName("");
+  };
+  const removeUser = (id) => {
+    const updateddata1 = data1.filter((item) => item.id !== id)
+    setData1(updateddata1);
+  }
   return (
     <div>
       <form className='form'>
@@ -7,14 +30,23 @@ const UserChallenge = () => {
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input' id='name' value={name} placeholder="Enter Name" onChange={(e) => setName(e.target.value)} />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type='button' className='btn btn-block' onClick={() => addUser(name)}>
           submit
         </button>
       </form>
-      {/* render users below */}
+      {/* render data1 below */}
+      <div className="row form">
+        {data1.map((item) => (
+          <div className="row" key={item.id}>
+            <div className="col-6"><h5>{item.name}</h5></div>
+            <div className="col-6"><button type="button" className="btn" style={{ marginLeft: '50px' }} onClick={() => removeUser(item.id)}>X</button></div>
+            <hr />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
